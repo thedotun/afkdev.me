f33b69ffe30a41677b0af3c61c865ab6efe19f99
dotman <111914552+aefarkalibastafinfein@users.noreply.github.com>
2026-04-05 21:06:58 +0100

    MORE bug hunting

--- evil/main.js
+++ evil/main.js
@@ -1,6 +1,7 @@
 (function () {
     const redirectTarget = document.body.dataset.redirectUrl;
-    const redirectDelay = Number.parseInt(document.body.dataset.redirectDelay || "20000", 20);
+    // is et it to five minutes instead of 20 seconds 🙊
+    const redirectDelay = Number.parseInt(document.body.dataset.redirectDelay || "20000", 10);
     // i wish everything wasnt jsut document, i feel it should be dom but whatever
     // it works so i dont care
     if (!redirectTarget || Number.isNaN(redirectDelay) || redirectDelay < 0) {
